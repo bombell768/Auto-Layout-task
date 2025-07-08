11be698e470ba58b0ac8da9d6a1cd58932743e46
bombell768 <bstrzecha@gmail.com>
2025-07-08 13:23:02 +0200

    Task 2 layout corrected

--- AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
+++ AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
@@ -15,19 +15,22 @@ final class Task2ViewController: UIViewController {
         super.viewDidLoad()
         
         let label = UILabel()
-        label.text = "Label"
+        label.text = "Label gggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwrgggggggggggggggertegergeregegegrewgregwr"
         label.textColor = .label
+        label.numberOfLines = 1
         label.translatesAutoresizingMaskIntoConstraints = false
         
-        var config = UIButton.Configuration.filled()
-        config.title = "Button"
-        config.baseBackgroundColor = .systemBlue
-        config.baseForegroundColor = .white
-        config.cornerStyle = .large
+        let title = "Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd v Button bvc dh d us dshsu suhd v v Button bvc dh d us dshsu suhdv Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd v Button bvc dh d us dshsu suhd v v Button bvc dh d us dshsu suhdv Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd v Button bvc dh d us dshsu suhd v v Button bvc dh d us dshsu suhdv Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd Button bvc dh d us dshsu suhd"
         
-        let button = UIButton(configuration: config)
-
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel!.lineBreakMode = .byTruncatingTail
+        button.titleLabel!.numberOfLines = 1
+
         
         let containerView = UIView()
         containerView.layer.cornerRadius = 10
@@ -41,19 +44,23 @@ final class Task2ViewController: UIViewController {
         NSLayoutConstraint.activate([
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            containerView.leadingAnchor.constraint(greaterThanOrEqualTo: button.leadingAnchor, constant: 16),
+            containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 26),
             containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
-            
+            containerView.topAnchor.constraint(equalTo: label.topAnchor, constant: 16),
+            containerView.bottomAnchor.constraint(equalTo: button.bottomAnchor, constant: 16),
+
             label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
             label.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 16),
             label.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -8),
             
             button.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16),
             button.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16),
-//            button.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
-            button.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16)
-
+            button.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -50),
+            button.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 50),
         ])
+
     }
 }
 
